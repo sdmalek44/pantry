c627c8faa4121591e4ecac504e9378a92c1b571a
Stephen Malek <malek.stephen44@gmail.com>
2018-06-05 09:11:01 -0600

    finish iteration one

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,15 @@
 class Pantry
+  attr_reader :stock
 
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def restock(item, amount)
+    @stock[item] += amount
+  end
+
+  def stock_check(item)
+    @stock[item]
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+  end
 
+  def test_it_has_attributes
+    expected = {}
+    assert_equal expected, @pantry.stock
+
+    assert_equal 0, @pantry.stock_check("Cheese")
+
+    @pantry.restock("Cheese", 10)
+    assert_equal 10, @pantry.stock_check("Cheese")
+
+    @pantry.restock("Cheese", 20)
+    assert_equal 30, @pantry.stock_check("Cheese")
+  end
 end
