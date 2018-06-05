0621f47cbbd7f497f4758bdec9eb0468ce44358b
Stephen Malek <malek.stephen44@gmail.com>
2018-06-05 11:58:05 -0600

    try to match spec harness exactly

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -5,7 +5,7 @@ require './lib/recipe'
 class PantryTest < Minitest::Test
   def setup
     @pantry = Pantry.new
-    
+
     @r1 = Recipe.new("Cheese Pizza")
     @r1.add_ingredient("Cheese", 20)
     @r1.add_ingredient("Flour", 20)
@@ -29,7 +29,7 @@ class PantryTest < Minitest::Test
 
     assert_equal 0, @pantry.stock_check("Cheese")
 
-    assert_equal 10, @pantry.restock("Cheese", 10)
+    @pantry.restock("Cheese", 10)
     assert_equal 10, @pantry.stock_check("Cheese")
 
     @pantry.restock("Cheese", 20)
@@ -40,6 +40,9 @@ class PantryTest < Minitest::Test
     @pantry = Pantry.new
 
     @r = Recipe.new("Cheese Pizza")
+    expected = {}
+    assert_equal expected, @r.ingredients
+
     @r.add_ingredient("Cheese", 20)
     @r.add_ingredient("Flour", 20)
 
@@ -49,12 +52,12 @@ class PantryTest < Minitest::Test
     @pantry.add_to_shopping_list(@r)
     assert_equal expected, @pantry.shopping_list
 
-    @r_2 = Recipe.new("Spaghetti")
-    @r_2.add_ingredient("Spaghetti Noodles", 10)
-    @r_2.add_ingredient("Marinara Sauce", 10)
-    @r_2.add_ingredient("Cheese", 5)
+    @r = Recipe.new("Spaghetti")
+    @r.add_ingredient("Spaghetti Noodles", 10)
+    @r.add_ingredient("Marinara Sauce", 10)
+    @r.add_ingredient("Cheese", 5)
 
-    @pantry.add_to_shopping_list(@r_2)
+    @pantry.add_to_shopping_list(@r)
     expected = {"Cheese" => 25, "Flour" => 20, "Spaghetti Noodles" => 10, "Marinara Sauce" => 10}
     assert_equal expected, @pantry.shopping_list
 
